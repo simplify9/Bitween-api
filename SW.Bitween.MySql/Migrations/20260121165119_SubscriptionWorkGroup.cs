4a32829af5c4af4163df6db9775250c2969965fc
Muhannad Al-Khatib <mmalkhatib@hotmail.com>
2026-01-21 20:08:10 +0300

    Add WorkGroupId to Subscriptions and create WorkGroup table in MySql migration

---
+++ SW.Bitween.MySql/Migrations/20260121165119_SubscriptionWorkGroup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SW.Bitween.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubscriptionWorkGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkGroupId",
+                table: "Subscriptions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "WorkGroup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    BusMessageName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Options = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkGroup", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_WorkGroupId",
+                table: "Subscriptions",
+                column: "WorkGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_WorkGroup_WorkGroupId",
+                table: "Subscriptions",
+                column: "WorkGroupId",
+                principalTable: "WorkGroup",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_WorkGroup_WorkGroupId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropTable(
+                name: "WorkGroup");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_WorkGroupId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "WorkGroupId",
+                table: "Subscriptions");
+
+        }
+    }
+}
