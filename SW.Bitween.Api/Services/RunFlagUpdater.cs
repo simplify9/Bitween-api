e5c43fbae0c96ac61a6268f951a0ec02058fc2cd
Muhannad Al-Khatib <mmalkhatib@hotmail.com>
2026-01-26 16:30:10 +0300

    Fix SQL update statement for PostgreSQL to reference the correct subscription table

--- SW.Bitween.Api/Services/RunFlagUpdater.cs
+++ SW.Bitween.Api/Services/RunFlagUpdater.cs
@@ -21,7 +21,7 @@ namespace SW.Bitween
         {
             var sqlUpdate = _dbType.ToLower() switch
             {
-                "pgsql" => $@"UPDATE Bitween.subscription SET is_running = true
+                "pgsql" => $@"UPDATE infolink.subscription SET is_running = true
                         WHERE id = '{id}' and is_running = false
                         RETURNING is_running",
                 "mssql" => $@"UPDATE Subscriptions SET IsRunning = 1
