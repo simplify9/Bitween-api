23ec42ad2254bd1db31964c6bbce81bf691bcc10
Muhannad Al-Khatib <mmalkhatib@hotmail.com>
2026-07-07 15:30:11 +0300

    fix merge conflict

--- SW.Bitween.Api/Domain/Subscription/Subscription.cs
+++ SW.Bitween.Api/Domain/Subscription/Subscription.cs
@@ -39,7 +39,7 @@ public class Subscription : BaseEntity
         type, null)
     {
         Inactive = true;
-        if (type != SubscriptionType.GatewayApiCall)
+        if (type != SubscriptionType.GatewayApiCall && type != SubscriptionType.BusGateway)
             throw new ArgumentException();
     }
     private Subscription(string name, int documentId, SubscriptionType type, int? partnerId = null,
