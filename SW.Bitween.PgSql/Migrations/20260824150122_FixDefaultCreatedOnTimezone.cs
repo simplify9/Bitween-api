0a715e615666908e7cfb1d5e1e6924fa89b14049
Hamza Alqurneh <hamza@simplify9.com>
2026-08-24 18:04:37 +0300

    fix: stop the default seed timestamp from depending on the machine's local timezone

---
+++ SW.Bitween.PgSql/Migrations/20260824150122_FixDefaultCreatedOnTimezone.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SW.Bitween.PgSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDefaultCreatedOnTimezone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Accounts",
+                keyColumn: "id",
+                keyValue: 9999,
+                column: "created_on",
+                value: new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "created_on",
+                value: new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "created_on",
+                value: new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "created_on",
+                value: new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Accounts",
+                keyColumn: "id",
+                keyValue: 9999,
+                column: "created_on",
+                value: new DateTime(2021, 12, 31, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "created_on",
+                value: new DateTime(2021, 12, 31, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "created_on",
+                value: new DateTime(2021, 12, 31, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                schema: "infolink",
+                table: "Roles",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "created_on",
+                value: new DateTime(2021, 12, 31, 22, 0, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
